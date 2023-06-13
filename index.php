f642a72b9c60375e68ad51f5f634c03c74392f81
Gianeh <pietropianigiani2@gmail.com>
2023-06-13 20:27:15 +0200

    html to php

--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 
 <head>
     <title>AOS Test 1 ciao come va?</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid black;
+            width: 20px;
+            height: 20px;
+        }
+
+        .square {
+            width: 20px;
+            height: 20px;
+        }
 </head>
 
 <body>
@@ -12,9 +27,9 @@
         <!--A 30 x 30 table-->
         <?php
         for ($i = 0; $i < 30; $i++) {
-            echo "<tr>";
+            echo "<tr class='square'>";
             for ($j = 0; $j < 30; $j++) {
-                echo "<td>0</td>";
+                echo "<td class='square'><-></td>";
             }
             echo "</tr>";
         }
