ab74da45fb55288cb6bc7b558d70699d3bd8821d
jimmy <jimmy.carapelli@gmail.com>
2023-06-15 18:14:30 +0200

    ss

--- Client_side/handler_map.js
+++ Client_side/handler_map.js
@@ -17,7 +17,7 @@ export function onLoad() {
         cell.innerHTML = "P";                   // Set cell content to "P"
         cell.classList.remove("square");        // Remove square class
         cell.classList.add("playerVillage");    // Add player-village class
-        cell.id = "player-village";             // Set the id of the cell to "player-village"
+        cell.id = "playerVillage";             // Set the id of the cell to "player-village"
       }
     }
   }
