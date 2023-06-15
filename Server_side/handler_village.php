25ecdbf8ac242d4516714ce303df2f58c8ada1f3
Gianeh <pietropianigiani2@gmail.com>
2023-06-15 12:57:26 +0200

    chache class

--- Server_side/handler_village.php
+++ Server_side/handler_village.php
@@ -1,86 +1,18 @@
 <?php
     //log the error
     ini_set('display_errors', 1);
-
-    // the session is sterted to keep track of unique csrf tokens
+    // include the chache class
+    include_once('redis_cache.php');
+    // the session is started to keep track of unique csrf tokens
     session_start();
     // generate the token
     if (!isset($_SESSION["csrf_token"])) {
         $_SESSION["csrf_token"] = bin2hex(random_bytes(32));
     }
 
-    // include the class files
-    include_once('structures.php');
-    include_once('player.php');
+    // create a new cache object
+    $cache = new Cache(array("player", "structures"));
+    $data = $cache->acquireData($_POST["data"], $_SESSION["csrf_token"]);
 
-    // other inclusions here .........
-
-
-    // instantiate a minimal cache object and save not already existing data
-    $redis = new Redis();
-    $redis->connect('AOS-Web-Testing.redis.cache.windows.net', 6379);
-    $redis->auth('1QJ5aC5vpZESy1MYbw5oU42lnMixqxm0PAzCaHl4QH4='); // password for redis server
-
-    // get the requested data from frontend (handler.js)
-    $requestedData = $_POST["data"];
-    // create the cache key based on the requested data and the csrf token
-    $cache_key = $requestedData . "_data_" . $_SESSION["csrf_token"];
-    //$cache_key = $requestedData;
-
-    // check if the requested data is already in the cache otherwise get it from the database and save it in the cache
-    $data = $redis->get($cache_key);
-    if($data == false){
-        // data not found in cache, instantiate object and store data in cache
-        switch ($requestedData) {
-
-            // ###############################################
-            // # Data should be generated and added to cache #
-            // # Based on the Database records but now we    #
-            // # Just create a new object from scratch       #
-            // # every time                                  #
-            // ###############################################
-
-            case "player":
-                $player = new Player("player1234", 5, 0, 0, 0, 0);
-                $data = $player->get_data();
-                break;
-
-            case "townhall":
-                $townhall = new Townhall(1, "townhall");
-                $data = $townhall->get_data();
-                break;
-
-            case "rockmine":
-                $rockmine = new Rockmine(1, "rockmine");
-                $data = $rockmine->get_data();
-                break;
-            
-            case "woodchopper":
-                $woodchopper = new Woodchopper(1, "woodchopper");
-                $data = $woodchopper->get_data();
-                break;
-            /*
-            case "woodchopper":
-                $school = new School(1, "school");
-                $data = $school->getData();
-                break;
-            */
-            // add more cases for other objects as needed
-
-            default:
-                // handle error
-                $data = array("error" => "invalid data requested");
-                break;
-        }
-        // save the data in the cache - third parameter is the time to live in seconds
-        $redis->set($cache_key, json_encode($data), 3600);
-    } else {
-        // data found in cache, decode the JSON string
-        $data = json_decode($data, true);
-    }
-    
-    
-    //return the data to the frontend
-    //header("Content-Type: application/json");
     echo json_encode($data);
 
---
+++ Server_side/redis_cache.php
@@ -0,0 +1,76 @@
+<?php
+    ini_set('display_errors', 1);
+
+    // include the class files
+    include_once('structures.php');
+    include_once('player.php');
+
+    // other inclusions here .........
+
+    class Cache{
+        
+        public $redis = new Redis();
+
+        function __construct($classes, $server='AOS-Web-Testing.redis.cache.windows.net', $pass='1QJ5aC5vpZESy1MYbw5oU42lnMixqxm0PAzCaHl4QH4='){
+            // include every class that should be cached
+            for ($i = 0; $i < count($classes); $i++) {
+                include_once($classes[$i].".php");
+            }
+            // connect to the redis server
+            $redis->connect($server, 6379);
+            $redis->auth($pass); // password for redis server
+        }
+
+        public function acquireData($data, $token){
+            $cache_key = $data . "_data_" . $token;
+            $output = $this->redis->get($cache_key);
+
+            if($data == false){
+                // data not found in cache, instantiate object and store data in cache
+                switch ($data) {
+        
+                    // ###############################################
+                    // # Data should be generated and added to cache #
+                    // # Based on the Database records but now we    #
+                    // # Just create a new object from scratch       #
+                    // # every time                                  #
+                    // ###############################################
+        
+                    case "player":
+                        $player = new Player("player1234", 5, 0, 0, 0, 0);
+                        $data = $player->get_data();
+                        break;
+        
+                    case "townhall":
+                        $townhall = new Townhall(1, "townhall");
+                        $data = $townhall->get_data();
+                        break;
+        
+                    case "rockmine":
+                        $rockmine = new Rockmine(1, "rockmine");
+                        $data = $rockmine->get_data();
+                        break;
+                    
+                    case "woodchopper":
+                        $woodchopper = new Woodchopper(1, "woodchopper");
+                        $data = $woodchopper->get_data();
+                        break;
+
+                    // add more cases for other objects as needed
+        
+                    default:
+                        // handle error
+                        $data = array("error" => "invalid data requested");
+                        break;
+                }
+                // save the data in the cache - third parameter is the time to live in seconds
+                $this->redis->set($cache_key, json_encode($data), 3600);
+            } else {
+                // data found in cache, decode the JSON string
+                $data = json_decode($data, true);
+            }
+
+            return $output;
+
+        }
+    }
