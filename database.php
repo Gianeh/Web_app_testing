d263c84a0c172535cd37668bf379e0588ce26d40
jimmy <jimmy.carapelli@gmail.com>
2023-06-16 16:16:38 +0200

    s

--- database.php
+++ database.php
@@ -1,13 +1,14 @@
 <?php
-ini_set('display_errors', 1);
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:aos-database.database.windows.net,1433; Database = AOS_Database", "aosadmin", "AOSpassword!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "ciao porcodio funziono"
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
+    echo "ciao porcodio non funziono"
 }
 
 // SQL Server Extension Sample Code:
