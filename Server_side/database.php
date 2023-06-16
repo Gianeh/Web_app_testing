4894a0e8b422eddca708cd4e2cc990437fbd54f3
jimmy <jimmy.carapelli@gmail.com>
2023-06-16 16:01:58 +0200

    database first try

---
+++ Server_side/database.php
@@ -0,0 +1,16 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:aos-database.database.windows.net,1433; Database = AOS_Database", "aosadmin", "AOSpassword!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "aosadmin", "pwd" => "AOSpassword!", "Database" => "AOS_Database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:aos-database.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
