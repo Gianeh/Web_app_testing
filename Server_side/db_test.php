af7393bc2bad52764773a699546627143199a6a9
Gianeh <pietropianigiani2@gmail.com>
2023-06-17 13:07:26 +0200

    commit

---
+++ Server_side/db_test.php
@@ -0,0 +1,33 @@
+<?php
+    include_once("database_query.php");
+
+    // create 4 unique record for table Dummy_player_data
+    $db = new databaseQuery();
+
+    $db->insert("Dummy_player_data", "id, name, pass, lvl", "1, 'test', 'test', 1");
+    $db->insert("Dummy_player_data", "id, name, pass, lvl", "2, 'test2', 'test2', 1");
+    $db->insert("Dummy_player_data", "id, name, pass, lvl", "3, 'test3', 'test3', 1");
+    $db->insert("Dummy_player_data", "id, name, pass, lvl", "4, 'test4', 'test4', 1");
+
+    // retrieve the whole table from the database
+    $table = $db->RetriveData("Dummy_player_data", "*", "Username = 'test'");
+
+    // print the whole table html style
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>id</th>";
+    echo "<th>name</th>";
+    echo "<th>pass</th>";
+    echo "<th>lvl</th>";
+    echo "</tr>";
+    foreach ($table as $row) {
+        echo "<tr>";
+        echo "<td>".$row["id"]."</td>";
+        echo "<td>".$row["Username"]."</td>";
+        echo "<td>".$row["Password"]."</td>";
+        echo "<td>".$row["Level"]."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    // $table = array(row1 => array(user => test, pass => test, lvl => 1), row2 => array(user => test2, pass => test2, lvl => 1), row3 => array(user => test3, pass => test3, lvl => 1), row4 => array(user => test4, pass => test4, lvl => 1));
