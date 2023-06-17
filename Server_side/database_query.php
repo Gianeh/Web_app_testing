baaedd569970df6bc6241cf98a2dccf7e0c5dd6a
jimmy <jimmy.carapelli@gmail.com>
2023-06-17 02:53:46 +0200

    need to test everithing and check addoncache

---
+++ Server_side/database_query.php
@@ -0,0 +1,63 @@
+<?php
+
+class databaseQuery{
+
+    public $conn;
+    function __construct($server, $user, $pass, $db){  //connect to the database
+        $this->conn = new PDO("sqlsrv:server = $server; Database = $db", "$user", "$pass");
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    // afunction the create a complitly new row
+    public function insert($table, $columns, $values){       // insert a new row in the table colums = "column1, column2, column3" values = "value1, value2, value3"
+        $query = "INSERT INTO $table ($columns) VALUES ($values)";
+        $this->conn->query($query);
+    }
+
+    // a function that update an already existing row
+    public function update($table, $columns, $values, $where){  
+        $query = "UPDATE $table SET $columns = $values WHERE $where";
+        $this->conn->query($query);
+    }
+    // a function that retrive data from the database
+    public function RetriveData($table, $columns, $where){
+        $query = "SELECT $columns FROM $table WHERE $where";
+        $stmt = $this->conn->query($query);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+    // a function that delete a row from the database
+    public function remove($table, $where){
+        $query = "DELETE FROM $table WHERE $where";
+        $this->conn->query($query);
+    }
+
+}
+
+
+// PHP Data Objects(PDO) Sample Code:
+/*
+try {
+    $conn = new PDO("sqlsrv:server = tcp:aos-database.database.windows.net,1433; Database = AOS_Database", "aosadmin", "AOSpassword!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "SELECT * FROM Dummy_player_data";
+    $stmt = $conn->query($query);
+    
+    // Fetch the results
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // Access the data
+        $name = $row['Username'];
+        $pwd = $row['Password'];
+        // ...access other columns here
+        
+        // Do something with the data
+        echo "Name: $name, password $pwd<br>";
+    }
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    echo "ciao porcodio non funziono";
+    die(print_r($e));
+}
+
+?>
--- Server_side/redis_cache.php
+++ Server_side/redis_cache.php
@@ -1,15 +1,21 @@
 <?php
 
-    // create function to delete chache record
-
     // create function for the switch adn the db query
 
 
     ini_set('display_errors', 1);
+    include_once('database_query.php');
+
+    // information needed to connect to the database
+    $server = "tcp:AOS-Web-Testing.database.windows.net,1433";
+    $db     = "AOS-Web-Testing";
+    $user   = "aosadmin";
+    $pass   = "AOSpassword!"; 
 
     class Cache{
         
         public $redis;
+        public $db;       // the database query object need to stay here?
 
         function __construct($classes, $server='AOS-Web-Testing.redis.cache.windows.net', $pass='1QJ5aC5vpZESy1MYbw5oU42lnMixqxm0PAzCaHl4QH4='){
             // include every class that should be cached
@@ -20,10 +26,46 @@
             $this->redis = new Redis();
             $this->redis->connect($server, 6379);
             $this->redis->auth($pass); // password for redis server
+
+            // create a new istance for the database query
+            $this->db = new databaseQuery($server, $user, $pass, $db);
+
         }
 
-        // a function that delete element on cache on requirement
+        // a function that get data from teh db and add it to the cache
+        function addInCache($data){
+            switch ($data) {
+    
+                case "player":
+                    $player = $this->db->RetriveData("Dummy_player_data", "*", "Username = 'test'");
+                    $output = $player->get_data();
+                    break;
+    
+                case "townhall":
+                    $townhall = new Townhall(1, "townhall");
+                    $output = $townhall->get_data();
+                    break;
+    
+                case "rockmine":
+                    $rockmine = new Rockmine(1, "rockmine");
+                    $output = $rockmine->get_data();
+                    break;
+                
+                case "woodchopper":
+                    $woodchopper = new Woodchopper(1, "woodchopper");
+                    $output = $woodchopper->get_data();
+                    break;
 
+                // add more cases for other objects as needed
+    
+                default:
+                    // handle error
+                    $output = array("error" => "invalid data requested");
+                    break;
+            }
+        }
+
+      
 
         public function acquireData($data, $token){
             if(!is_string($data)){
@@ -39,42 +81,7 @@
 
             if($output == false){
                 // data not found in cache, instantiate object and store data in cache
-                switch ($data) {
-        
-                    // ###############################################
-                    // # Data should be generated and added to cache #
-                    // # Based on the Database records but now we    #
-                    // # Just create a new object from scratch       #
-                    // # every time                                  #
-                    // ###############################################
-        
-                    case "player":
-                        $player = new Player("player1234", 5, 0, 0, 0, 0);
-                        $output = $player->get_data();
-                        break;
-        
-                    case "townhall":
-                        $townhall = new Townhall(1, "townhall");
-                        $output = $townhall->get_data();
-                        break;
-        
-                    case "rockmine":
-                        $rockmine = new Rockmine(1, "rockmine");
-                        $output = $rockmine->get_data();
-                        break;
-                    
-                    case "woodchopper":
-                        $woodchopper = new Woodchopper(1, "woodchopper");
-                        $output = $woodchopper->get_data();
-                        break;
-
-                    // add more cases for other objects as needed
-        
-                    default:
-                        // handle error
-                        $output = array("error" => "invalid data requested");
-                        break;
-                }
+               $output = addInCache($data);
                 // save the data in the cache - third parameter is the time to live in seconds
                 $this->redis->set($cache_key, json_encode($output), 3600);
             } else {
@@ -85,4 +92,24 @@
             return $output;
 
         }
+
+
+        //a function that delete something from the cache by the key
+        public function deleteData($data, $token){
+            if($this->redis === null){
+                // Redis connection not established
+                return array("error" => "Redis connection not established");
+            }
+            $cache_key = $data . "_data_" . $token;   // the key is the data name + the token
+            $deleted = $this->redis->del($cache_key); // del returns TRUE or FALSE
+            if($deleted){
+                return array("key '$data' deleted successfuly from redis cache");
+            }else{
+                return array("key '$data' deleted successfuly from redis cache");
+            }
+
+        }
+
+      
+
     }
