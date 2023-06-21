9824806ee77106febbaf7ebdd44e48e6a39839f5
Gianeh <pietropianigiani2@gmail.com>
2023-06-21 13:23:04 +0200

    try again

--- Client_side/Village/shortcuts.js
+++ Client_side/Village/shortcuts.js
@@ -1,9 +1,22 @@
 export function hideCheats() {
     var cheatDiv = document.getElementById("cheat_section");
-    cheatDiv.style.display = "inline-block";
+    cheatDiv.style.display = "none";
+    // acquire every element in the cheat section div
+    var cheatElements = cheatDiv.getElementsByTagName("*");
+    // make every element in the cheat section div invisible
+    for (var i = 0; i < cheatElements.length; i++) {
+        cheatElements[i].style.display = "none";
+    }
 }
 
 export function showCheats() {
+    // make every element in the cheat section div visible
     var cheatDiv = document.getElementById("cheat_section");
-    cheatDiv.style.display = "none";
+    cheatDiv.style.display = "inline-block";
+    // acquire every element in the cheat section div
+    var cheatElements = cheatDiv.getElementsByTagName("*");
+    // make every element in the cheat section div visible
+    for (var i = 0; i < cheatElements.length; i++) {
+        cheatElements[i].style.display = "inline-block";
+    }
 }
