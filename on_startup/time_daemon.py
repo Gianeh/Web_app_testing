0d51dabca3f8ea735ee40520da537431a7e14b2b
Gianeh <pietropianigiani2@gmail.com>
2023-06-22 19:36:41 +0200

    time daemon

--- on_startup/time_daemon.py
+++ on_startup/time_daemon.py
@@ -1,27 +1,36 @@
-# time updates can be handled by a python process -- C++ would be efficient but simplicity over efficiency
+# time updates can be handled by a python process -- C++ would be efficient but simplicity over efficien>
 # this process will be started by the main process and will run in the background
 
-import time
+import schedule
 from datetime import datetime as dt
 # s library to send sql commands to the database
 import pyodbc
+import time
 
+def request():
+        # create a connection to the database
+        conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=tcp:aos-database.database.windows.net;'
+        'DATABASE=AOS_Database;'
+        'UID=aosadmin;'
+        'PWD=AOSpassword!'
+        )
+        time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO time (t) VALUES (?)", time)
+        '''
+        results = cursor.fetchall()
+        for row in results:
+                print(row)
+        '''
 
-# create a connection to the database
-conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=tcp:aos-database.database.windows.net,1433;'
-    'DATABASE=yourdatabase;'
-    'UID=aosadmin;'
-    'PWD=AOSpassword!'
-    )
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM users")
+schedule.every(1).seconds.do(request)
 
-results = cursor.fetchall()
-for row in results:
-    print(row)
-
-cursor.close()
-conn.close()
+while True:
+        schedule.run_pending()
+        
