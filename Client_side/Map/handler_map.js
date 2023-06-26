e3b365eb073b62d8197c3dc8362dbafc0faedc4d
jimmy <jimmy.carapelli@gmail.com>
2023-06-26 22:07:24 +0200

    s

--- Client_side/Map/handler_map.js
+++ Client_side/Map/handler_map.js
@@ -16,6 +16,9 @@ export function onLoad() {
   let enemypos = getDataWithParameter("player", "x<60 AND y<60 AND username  <> '"+player["username"]+"'", "x, y, username")      // search in local cache enemy data
   console.log(enemypos);
 
+  let container = document.getElementById("container");
+  container.innerHTML = "Loading War Map for " + player["username"] + "!";
+
  // create the table
   for (var i = 0; i < 500; i++) {
     var row = table.insertRow();
