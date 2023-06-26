3fd69949e995c603171780ba9477b2ac8da57a52
OverGravy <jimmy.carapelli@example.com>
2023-06-26 17:25:31 +0200

    yahy

--- Client_side/Map/handler_map.js
+++ Client_side/Map/handler_map.js
@@ -8,7 +8,7 @@ export function onLoad() {
   playerpos = pickRecords(playerpos, ["x", "y"]);
   console.log(playerpos["x"], playerpos["y"]);
 
-  let enemypos = getDataWithParameter("player", "x<10 AND y<10", "x, y")      // search in local cache enemy data
+  let enemypos = getDataWithParameter("player", "x<60 AND y<60", "x, y")      // search in local cache enemy data
   console.log(enemypos);
 
   for (var i = 0; i < 100; i++) {
