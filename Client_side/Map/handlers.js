0f38726ec6fd041c1073c020af243cf4d8523fef
jimmy <jimmy.carapelli@gmail.com>
2023-06-27 21:42:26 +0200

    a

--- Client_side/Map/handlers.js
+++ Client_side/Map/handlers.js
@@ -3,7 +3,7 @@ export function PlayerFocus(cellSize,x,y) {
 
   let container = document.getElementById("canvasContainer");
 
-  container.scrollTo((cellSize*x)*2,(cellSize*y)*2) // Translate the canvas
+  container.scrollTo((cellSize*x),(cellSize*y)) // Translate the canvas
 }
 
 export function setHandlers(x,y,cellSize) {
