668c20373f429d3e855676de8e6fcf1c760b4e5e
jimmy <jimmy.carapelli@gmail.com>
2023-07-11 12:59:26 +0200

    commit

--- Client_side/Map/Graphical_function.js
+++ Client_side/Map/Graphical_function.js
@@ -6,98 +6,174 @@
 // The concept byond drawing is putting important things at the end to avoid overwriting
 
 // 1) draw the map
-// 2) draw the rocks, trees, whater
+// 2) draw the rocks, trees, water
 // 3) draw the player village and enemy villages
 
+
+const CryptoJS = require('crypto-js');
+
 // Function to draw rocks inside the map
-export function drawRock() {
+export function drawLandScape() {
 
-    // max e min number of rock spawn
-    const MAX_ROCKS = 5;  // MAX Number of rocks per row
-    const MIN_ROCKS = 2;  // MIN Number of rocks per row
-    const DENSITY = 5;    // Number Of row with rocks
+    /*
 
-    const MIN_ROWS_DISTANCE = 5; // Minimum distance between rows with rocks
+    // DEFAULT VALUE
+    let number_of_rocks = 3;  // Number of rocks per row
+    let number_of_trees = 5;  // Number Of trees per row
+    let number_of_water = 2;  // Number Of water per row
+    let density = 12;         // Number Of row with element
 
-    // get the table
-    let table = document.getElementById("WarMap");
+    // get current origin
+    let CurrentOrigin = localStorage.getItem("CurrentOrigin");
 
-    // get number of rows
-    let rows = localStorage.getItem("Rows");
-    let columns = localStorage.getItem("Cols");
+    // realize the seed using hash SHA1 on current origin
+    var hash = CryptoJS.SHA1(CurrentOrigin["x"] + CurrentOrigin["y"]);
+    var hex = hash.toString(CryptoJS.enc.Hex);
+    hash = hex.replace(/\D/g, '');
 
-    // randomize the number of rocks per Row 
-    let rocksPerRow = Math.floor(Math.random() * (MAX_ROCKS - MIN_ROCKS + 1)) + MIN_ROCKS;
-    let rowsWithRocks = [];
+    console.log("hash seed: ", hash);
 
-    console.log("rocksPerRow", rocksPerRow);
+    // get rocks position
+    let rocksPosition = [];
+    for (let i = 0; i < number_of_rocks; i++) {
+        rocksPosition[i] = parseInt(input.substring(0+i, 2+i), 10);
+    }
 
-    // randomixe the number of rows with rocks
-    for (let i = 0; i < DENSITY; i++) {
-        rowsWithRocks[i] = Math.floor(Math.random() * (rows - DENSITY + 1)) + DENSITY;
-        //check if row containing rocks is too close to the previous one
-        if ((rowsWithRocks[i] < (rowsWithRocks[i - 1] + MIN_ROWS_DISTANCE) && i != 0)) {
-            rowsWithRocks[i] = rowsWithRocks[i - 1] + MIN_ROWS_DISTANCE;
-        } else if ((rowsWithRocks[i] > (rowsWithRocks[i - 1] - MIN_ROWS_DISTANCE) && i != 0)) {
-            rowsWithRocks[i] = rowsWithRocks[i - 1] - MIN_ROWS_DISTANCE;
+    //check if the number of rockposition is usable
+    for(let i = 0; i < number_of_rocks; i++){
+        if(rocksPosition[i] = 0){
+            rocksPosition[i] = ;
+        }
+        if(rocksPosition[i] > localStorage.getItem("Cols")){
+            rocksPosition[i] = ;
+        }
+        
+    }
+
+    console.log("rocks position: ", rocksPosition);
+
+    // get trees position
+    let treesPosition = [];
+    for (let i = 0; i < number_of_trees; i++) {
+        treesPosition[i] = parseInt(input.substring(0+(number_of_rocks*2)+i, 2+(number_of_rocks*2)+i), 10);
+    }
+
+    //check if the number of treesposition is usable
+    for(let i = 0; i < number_of_trees; i++){
+        if(treesPosition[i] = 0){
+            treesPosition[i] = ;
+        }
+        if(treesPosition[i] > localStorage.getItem("Cols")){
+            treesPosition[i] = ;
         }
     }
 
-    console.log("rowsWithRocks", rowsWithRocks);
+    console.log("trees position: ", treesPosition);
 
+    // get water position
+    let waterPosition = [];
+    for (let i = 0; i < number_of_water; i++) {
+        waterPosition[i] = parseInt(input.substring(0+(number_of_rocks*2)+(number_of_trees*2)+i, 2+(number_of_rocks*2)+(number_of_trees*2)+i), 10);
+    }
+
+    //check if the number of waterposition is usable
+    for(let i = 0; i < number_of_water; i++){
+        if(waterPosition[i] = 0){
+            waterPosition[i] = ;
+        }
+        if(waterPosition[i] > localStorage.getItem("Cols")){
+            waterPosition[i] = ;
+        }
+    }
+    
+    console.log("water position: ", waterPosition);
+
+    */
+
+    // idea: retrive x,y position from the hash and use them to draw landscape
+
+    // PROS 
+
+    // 1) using hash in this way i dont need to make complex controll
+    // 2) is faster than the other method
+
+    // PROBLEM 
+
+    // 1) using his method is very casual how rocks, trees and water are placed
+    // 2) x or y can exeeds the map size                                            ---> I NEED TO FIX THIS
+    
+    // SOLUTION
+
+    // max hash long is 42
+
+    const MAX_ROCKS = 3;
+    const MAX_TREES = 5;
+    const MAX_WATER = 2;
+
+     // get current origin
+     let CurrentOrigin = localStorage.getItem("CurrentOrigin");
+
+     // realize the seed using hash SHA1 on current origin
+     var hash = CryptoJS.SHA1(CurrentOrigin["x"] + CurrentOrigin["y"]);
+     var hex = hash.toString(CryptoJS.enc.Hex);
+     hash = hex.replace(/\D/g, '');
+ 
+     console.log("hash seed: ", hash);
+
+    // get rocks position;
     let rocksPosition = [];
+    for (let i = 0; i < MAX_ROCKS; i++) {
+        rocksPosition[i]["x"] = parseInt(hash.substring(0+i, 2+i), 10);
+        rocksPosition[i]["y"] = parseInt(hash.substring(2+i, 4+i), 10);
+    }
 
-    //Scroll every row inside the table
+    // get trees position
+    let treesPosition = [];
+    for (let i = 0; i < MAX_TREES; i++) {
+        treesPosition[i]["x"] = parseInt(hash.substring(0+i+(MAX_ROCKS*2), 2+i+(MAX_ROCKS*2)), 10);
+        treesPosition[i]["y"] = parseInt(hash.substring(6+i+(MAX_ROCKS*2), 8+i+(MAX_ROCKS*2)), 10);
+    }
+
+    // get water position
+    let waterPosition = [];
+    for (let i = 0; i < MAX_WATER; i++) {
+        waterPosition[i]["x"] = parseInt(hash.substring(0+i+(MAX_ROCKS*2)+(MAX_TREES*2), 2+i+(MAX_ROCKS*2)+(MAX_TREES*2)), 10);
+        waterPosition[i]["y"] = parseInt(hash.substring(10+i+(MAX_ROCKS*2)+(MAX_TREES*2), 12+i+(MAX_ROCKS*2)+(MAX_TREES*2)), 10);
+    }
+
+    // scroll all the table
     for (let i = 0; i < table.rows.length; i++) {
-        // is the right row?
-        for (let j = 0; j < DENSITY; j++) {
-            if (rowsWithRocks[j] == i) {
+        for (let j = 0; j < table.rows[i].cells.length; j++) {
+            // get the cell
+            let cell = table.rows[i].cells[j];
 
-                for (let k = 0; k < rocksPerRow; k++) {
-
-                    // randomize position of the rock
-                    let rockcell = Math.floor(Math.random() * columns);
-                    rocksPosition[k] = rockcell;
-
-                    // check if there was already a rock there  I NEED TO AVOID LOOPS
-                    let counter = 0;
-                    let different = true;
-                    while (different) {
-                        let check = 0;
-                        counter++;
-                        //check entire roksPosition if the new one is different
-                        for (let x = 0; x < rocksPosition.length; x++) {
-                            let element = rocksPosition[x];
-                            if (element == rockcell) {
-                                rockcell = Math.floor(Math.random() * columns);
-                                rocksPosition[k] = rockcell;
-                                break;
-                            } else {
-                                check++;
-                            }
-                        }
-
-                        if (check == rocksPosition.length - 1) {
-                            different = false;
-                        }
-                        if (counter == 100) { // if i can't find a different position i will stop
-                            different = false;
-                        }
-                    }
-                    console.log("rockposition: ", rocksPosition);
-                }
-
-                //Draw rocks inside the randomize chosen row
-
-                for (let x = 0; x < rocksPerRow; x++) {
-                    let cell = table.rows[i].cells[rocksPosition[x]];
-                    console.log("cell: ", i, " ", rocksPosition[x]);
+            // check if the cell is a rock
+            for (let k in rocksPosition) {
+                if (j  == rocksPosition[k]["x"] && i  == rocksPosition[k]["y"]) {
                     cell.className = "rock_cell";
+                    cell.id = "rock";
+                    cell.innerHTML = "R";
                 }
+            }
 
+            // check if the cell is a tree
+            for (let k in treesPosition) {
+                if (j == treesPosition[k]["x"] && i == treesPosition[k]["y"]) {
+                    cell.className = "tree_cell";
+                    cell.id = "tree";
+                    cell.innerHTML = "T";
+                }
+            }
+
+            // check if the cell is a water
+            for (let k in waterPosition) {
+                if (j  == waterPosition[k]["x"] && i == waterPosition[k]["y"]) {
+                    cell.className = "water_cell";
+                    cell.id = "water";
+                    cell.innerHTML = "W";
+                }
             }
         }
-
     }
 
 }
@@ -130,11 +206,5 @@ export function drawPlayerAndEnemy(CurrentOrigin, enemypos, player) {
             }
         }
     }
-
-}
-
-
-// a function that drow trees around the map
-export function drawTrees(){
 
 }
