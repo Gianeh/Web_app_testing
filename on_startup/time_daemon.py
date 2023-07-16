e66cdea7472dd422999567f8fd7bc4fdb409855a
Gianeh <pietropianigiani2@gmail.com>
2023-07-16 16:16:15 +0200

    update update update

--- on_startup/time_daemon.py
+++ on_startup/time_daemon.py
@@ -1,4 +1,4 @@
-# time updates can be handled by a python process -- C++ would be efficient but simplicity over efficien>
+# time updates can be handled by a python process -- C++ would be efficient but simplicity over efficiency
 # this process will be started by the main process and will run in the background
 
 import schedule
@@ -7,47 +7,70 @@ from datetime import datetime as dt
 import pyodbc
 import time
 
-def request():
-        # create a connection to the database
-        conn = pyodbc.connect(
+# define a global connection to the database
+def connect():
+        CONNECTION = pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=tcp:aos-database.database.windows.net;'
-        'DATABASE=AOS_Database;'
-        'UID=aosadmin;'
-        'PWD=AOSpassword!'
+        'SERVER=tcp:ageofstronghold.database.windows.net;'
+        'DATABASE=AgeofStronghold-database;'
+        'UID=AOSAdmin;'
+        'PWD=AOSPassword!'
         )
-        time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO time (t) VALUES (?)", time)
-        '''
-        results = cursor.fetchall()
-        for row in results:
-                print(row)
-        '''
+        return CONNECTION
 
-        conn.commit()
+
+def build_select_query(table, column, condition = ""):
+        query = "SELECT " + column + " FROM " + table + " WHERE " + condition
+        return query
+
+def build_insert_query(table, column, value):
+        query = "INSERT INTO " + table + " (" + column + ") VALUES (" + value + ")"
+        return query
+
+def build_update_query(table, columns, values, condition):
+        query = "UPDATE " + table + " SET "
+        for i in range(len(columns)):
+                query += columns[i] + " = " + values[i] + ", "
+        query = query[:-2]
+        query += " WHERE " + condition
+        return query
+
+def build_delete_query(table, condition):
+        query = "DELETE FROM " + table + " WHERE " + condition
+        return query
+
+def request(query):
+
+        cursor = connect().cursor()
+        cursor.execute(query)
+
+        # if query includes a select statement, return the result
+        if query[:6] == "SELECT":
+                result = cursor.fetchall()
+                cursor.close()
+                CONNECTION.commit()
+                CONNECTION.close()
+                return result
+
+        CONNECTION.commit()
         cursor.close()
-        conn.close()
+        CONNECTION.close()
 
+def update_upgrade_events():
+        # this function should query the database for every upgrade event of every user (user_id) and check if completion is due
+        # if so it should update the structures table accordingly using level field in the events table
+        query = build_select_query("events", "*", "event_type = 'upgrade'")
+
+def update_resource_events():
+        # this function should query the database for every resource event of every user (user_id) and check if completion is due
+        # if so it should update the resources table accordingly using ammount field in the requirements json file
+
+def update_training_events():
+        # this function should query the database for every training event of every user (user_id) and check if completion is due
+        # if so it should update the troops table accordingly using the event_type field in the events table and adding 1 to the corresponding troop type
 schedule.every(1).seconds.do(request)
 
 while True:
         schedule.run_pending()
         
-'''
-/// GLI UPGRADE POTREBBERO AVERE DEI REQUIREMENTS IN UN JSON CHE VIENE CONTROLLATO SIA IN BACKEND CHE IN FRONTEND PER AGGIORNARE
-/// I BOTTONI IN REAL TIME DI COLORI DIVERSI SE LE RICHIESTE SONO SODDISFATTE O MENO,
-/// IL CLICK DEVE TRIGGERARE DATA INTERFACE E A SUA VOLTA QUESTO DEVE AGGIUNGERE LA ACTION CON I DATI DI DURATA E FINE IN CACHE E DATABASE
-/// IL DATO IN CACHE PUò ESSERE USATO PER AGGIORNARE DEI COUNTDOWN IN FRONTEND
-/// VA STABILITO IL MECCANISMO DI CANCELLAZIONE DELLA ACTION IN CACHE, IL TIME DAEMON CONTROLLERà ANCHE CON USER OFFLINE OGNI X SECONDI
-/// SE CI SONO AZIONI GIà TERMINATE PRIMA CHE IL DAEMON SI ACC0RGA E L'UTENTE è ONLINE L'AGGIORNAMENTO DEVE ESSERE FATTO IN QUEL MOMENTO
-/// E LA ACTION RIMOSSA DA CACHE E DATABASE ---> HOW??
-'''
-
-
-'''
-This daemon needs to periodically check 3 things:
-1) if there are any action in the database that are expired -> update database tables accordingly
-2) if there are any action in a txt file that are relative to online players -> update database tables accordingly
-3) if a war starts -> make sure to immediately update database based on the txt file
-'''
+        time.sleep(1)
